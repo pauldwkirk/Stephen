c059c4205bb3ee07a8c7c7cfa20053196711f613
Coleman <stcolema@tcd.ie>
2018-07-23 15:34:59 +0100

    Added entropy window calculaiton and entropy plot (default values are perhaps dubious)

--- parameter_sampling.R
+++ parameter_sampling.R
@@ -264,14 +264,41 @@ point_similarity <- function(cluster_record) {
 entropy <- function(class_weights){
   # Measure of convergence, entropy from Information theory
   # Class weights: vector of class_weights (numbers from unit interval)
-  entropy <- sum(class_weights * log(class_weights))
+  entropy_components <- class_weights * log(class_weights)
+  entropy_components[is.nan(entropy_components)] <- 0
+  entropy <- sum(entropy_components)
+}
+
+entropy_window <- function(entropy_vec, 
+                           start = 1, 
+                           window_length = 20, 
+                           tolerance = 0.01
+                           ){
+  # Find the point at which entropy stabilises in the iterations
+  n <- length(entropy_vec)
+  search_range <- seq(from = start, to = n - window_length, by = window_length)
+  for(i in search_range){
+    win_1 <- entropy_vec[i : (i + window_length - 1)]
+    win_2 <- entropy_vec[(i + window_length) : min((i + 2 * window_length - 1), n)]
+    
+    mean_1 <- mean(win_1)
+    mean_2 <- mean(win_2)
+    
+    sd_1 <- sd(win_1)
+    sd_2 <- sd(win_2)
+    
+    if((abs(mean_1 - mean_2) < tolerance) + (abs(sd_1 - sd_2) < tolerance)){
+      return(i)
+    }
+    
+  }
 }
 
 # === Demo =====================================================================
 
 N <- 20
 k <- 2
-data <- c(rnorm(N / 2, -2, 1), rnorm(N / 2, 2, 1)) # hist(data)
+data <- c(rnorm(N / 2, -1, 1), rnorm(N / 2, 1, 1)) # hist(data)
 mu_0 <- 0
 df_0 <- 1
 scale_0 <- matrix(1)
@@ -285,12 +312,13 @@ class_labels <- sample(c(1, 2), N, replace = T)
 class_labels_0 <- class_labels
 
 num_iter <- 1000
-burn <- 100
+burn <- 0
 record <- matrix(0, nrow = N, ncol = num_iter - burn)
+entropy_cw <- rep(0, num_iter)
 
 for (qwe in 1:num_iter) {
   class_weights <- class_weight_posterior(concentration_0, class_labels, k)
-
+  entropy_cw[qwe] <- entropy(class_weights)
   for (j in 1:k) {
     cluster_data <- data[class_labels == j]
 
@@ -311,10 +339,32 @@ for (qwe in 1:num_iter) {
   }
 }
 
-plot_data <- data.frame(X = data, Index = 1:N, Class = class_labels)
-ggplot(plot_data, aes(x = X, y = Index, colour = Class)) + geom_point()
-
 sim <- point_similarity(record)
+
+entropy_data <- data.frame(Index = 1:num_iter, Entropy = entropy_cw)
+
+burn <- entropy_window(entropy_cw)
+
+entropy_plot <- ggplot(data = entropy_data, mapping = aes(x = Index, y = Entropy)) +
+  geom_point() + 
+  geom_vline(mapping = aes(xintercept = burn, colour = "Burn"), lty = 2) +
+  # geom_smooth(se = F) +
+  # geom_line() + 
+  ggtitle("Entropy over iterations including recommended burn") +
+  xlab("Iteration") + ylab("Entropy") + 
+  scale_color_manual(name = "", values = c(Burn = "red")) +
+  NULL
+
+
+
 pheatmap(sim) # similarity
 pheatmap(1 - sim) # dissimilarity
-hist(data) # look at the data
+
+# look at the data
+hist(data)
+plot_data <- data.frame(X = data, Index = 1:N, Class = class_labels)
+ggplot(plot_data, aes(x = X, y = Index, colour = Class)) + geom_point()
+x <- entropy(class_weights)
+
+entropy_plot
+burn
