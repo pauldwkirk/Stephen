3695b5049b4c38e3ae41406fa8f365ef1d5fc2ec
Stephen Coleman <34100773+stcolema@users.noreply.github.com>
2018-08-01 10:07:12 +0100

    Adding more functions from R script; not currently working and quite messy.

--- sampleRcpp.cpp
+++ sampleRcpp.cpp
@@ -61,3 +61,109 @@ NumericMatrix similarity_mat(NumericMatrix cluster_record){
   }
   return out;
 }
+
+// [[Rcpp::export]]
+NumericMatrix point_comparison(int num_iter,
+                               NumericVector concentration_0,
+                               NumericVector class_labels,
+                               NumericMatrix data,
+                               float df_0,
+                               int k,
+                               int burn,
+                               int thinning
+){
+  int N = data.nrow();
+  NumericMatrix record(N, num_iter - burn);
+  NumericVector entropy_cw(num_iter);
+  NumericMatrix sim;
+    
+  NumericVector class_weights;
+
+  Rcpp::List variance;
+  Rcpp::List mu;
+
+  for(int i = 0; i < num_iter; i++){
+
+    class_weights = class_weight_posterior(concentration_0, class_labels, k);
+    entropy_cw(i) = entropy(class_weights)
+    for (int j = 0; j < k; j++) {
+      cluster_data <- as.data.frame(data[class_labels == j, ])
+      
+      
+      variance[[j]] <- variance_posterior(
+          df_0,
+          scale_0,
+          lambda_0,
+          mu_0,
+          cluster_data
+      )
+      
+      mu[[j]] <- mean_posterior(mu_0, variance[[j]], lambda_0, cluster_data)
+      */
+    
+    }
+    
+    for (int jj = 0; jj < N; jj++){
+      /***R
+      point <- data[jj, ]
+      
+      class_labels[jj] <- sample_class(point, data, k, class_weights, class_labels,
+                                          mu = mu,
+                                          variance = variance
+      )
+
+    
+      */
+    }
+    if (i > burn && (i - burn) % thinning == 0) {
+      /***R
+      record[, i - burn] <- t(class_labels)
+      */
+      
+    }
+  }
+  
+  sim = similarity_mat(record);
+  return sim;
+}
+
+
+
+
+
+                               
+NumericVector class_weight_posterior(float concentration_0,
+                                     NumericVector class_labels,
+                                     int k){
+  NumericVector class_weight(k);
+  int n = class_labels.size();
+  int class_count = 0;
+  float concentration = 0;
+  float total_class_weight = 0;
+  for (int i = 0; i < k; i++) {
+    for (int j = 0; j < n; j++ )
+    if (class_labels(j) == i) {
+      class_count++;
+    }
+    concentration = concentration_0 + class_count;
+    class_weight(i) = rgamma(1, concentration, 1)(1);
+    total_class_weight += class_weight(i);
+  }
+  for (int i = 0; i < k; i++) {
+    class_weight(i) = class_weight(i) / total_class_weight;
+  }
+  return class_weight;
+}
+
+float entropy(NumericVector class_weights){
+  
+}
+
+
+entropy <- function(class_weights) {
+# Measure of convergence, entropy from Information theory
+# Class weights: vector of class_weights (numbers from unit interval)
+  entropy_components <- class_weights * log(class_weights)
+  entropy_components[is.nan(entropy_components)] <- 0
+  entropy <- sum(entropy_components)
+}
